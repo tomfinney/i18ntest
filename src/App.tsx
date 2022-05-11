21c0783b6937d925dbbf9a5a042b8ea7fb4028b1
Tom Finney <23354981+tomfinney@users.noreply.github.com>
2022-05-11 12:50:29 +0100

    add typesafe-i18n to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,77 @@
 /** @jsx jsx */
-import { css, jsx, Global } from "@emotion/react"
-import { colors, theme } from "./constants/theme"
-import { Router } from "./Router"
+import { css, jsx, Global } from "@emotion/react";
+import { colors, theme } from "./constants/theme";
+import { Router } from "./Router";
+import TypesafeI18n from "./i18n/i18n-react";
+
+import { loadAllLocales } from "./i18n/i18n-util.sync";
+
+// boo
+loadAllLocales();
 
 export function App() {
   return (
-    <div>
-      <Global
-        styles={css`
-          @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap");
+    <TypesafeI18n locale="en">
+      <div>
+        <Global
+          styles={css`
+            @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap");
 
-          * {
-            box-sizing: border-box;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-          }
-
-          body {
-            color: ${theme.text.paragraph.color};
-            margin: 0;
-            font-size: 100%;
-            font-family: Lato, sans-serif;
-            line-height: 1.6;
-            background-color: ${colors.white};
-          }
-
-          a {
-            text-decoration: none;
-          }
-
-          b,
-          strong {
-            color: ${theme.form.text.color};
-            font-weight: ${theme.form.text.weight};
-          }
-
-          label {
-            display: block;
-            color: ${theme.form.text.color};
-            font-weight: ${theme.form.text.weight};
-            margin-bottom: 0.75em;
-          }
-
-          input {
-            border: 2px solid ${theme.form.border.color};
-            border-radius: 0;
-            padding: 6px;
-            color: ${theme.form.text.color};
-            font-weight: ${theme.form.text.weight};
-            font-size: 100%;
-
-            &:focus {
-              outline: 1px solid ${theme.form.outline.color};
+            * {
+              box-sizing: border-box;
+              -webkit-font-smoothing: antialiased;
+              -moz-osx-font-smoothing: grayscale;
             }
-          }
-        `}
-      ></Global>
-      <div
-        css={css`
-          min-height: 100vh;
-        `}
-      >
-        <Router />
+
+            body {
+              color: ${theme.text.paragraph.color};
+              margin: 0;
+              font-size: 100%;
+              font-family: Lato, sans-serif;
+              line-height: 1.6;
+              background-color: ${colors.white};
+            }
+
+            a {
+              text-decoration: none;
+            }
+
+            b,
+            strong {
+              color: ${theme.form.text.color};
+              font-weight: ${theme.form.text.weight};
+            }
+
+            label {
+              display: block;
+              color: ${theme.form.text.color};
+              font-weight: ${theme.form.text.weight};
+              margin-bottom: 0.75em;
+            }
+
+            input {
+              border: 2px solid ${theme.form.border.color};
+              border-radius: 0;
+              padding: 6px;
+              color: ${theme.form.text.color};
+              font-weight: ${theme.form.text.weight};
+              font-size: 100%;
+
+              &:focus {
+                outline: 1px solid ${theme.form.outline.color};
+              }
+            }
+          `}
+        ></Global>
+
+        <div
+          css={css`
+            min-height: 100vh;
+          `}
+        >
+          <Router />
+        </div>
       </div>
-    </div>
-  )
+    </TypesafeI18n>
+  );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,32 @@
 /** @jsx jsx */
-import { css, jsx } from "@emotion/react"
+import { css, jsx } from "@emotion/react";
+import { useContext, useEffect } from "react";
+import { I18nContext } from "../i18n/i18n-react";
+import { Locales } from "../i18n/i18n-types";
 
 export function HomePage() {
+  const thing = useContext(I18nContext);
+
+  useEffect(() => {
+    thing.setLocale("en");
+  }, []);
+
   return (
     <div
       css={css`
-        max-width: 1300px;
+        max-width: 500px;
         margin: 0 auto;
         min-height: 100vh;
       `}
     >
       <h1>test</h1>
+      <select onChange={(e) => thing.setLocale(e.target.value as Locales)}>
+        <option value="en">en</option>
+        <option value="de">de</option>
+      </select>
+
+      <p>{thing.LL.HI({ name: "aaa" })}</p>
+      <p>{thing.LL.b()}</p>
     </div>
-  )
+  );
 }
